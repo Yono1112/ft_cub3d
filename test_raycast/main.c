2f7347a5afaab922a8c11be25997b20efc510885
Yono1112 <yuma.ohno.sss@gmail.com>
2023-08-17 15:41:14 +0900

    change worldmap data type from int to char and create sturct t_mlx

--- test_raycast/main.c
+++ test_raycast/main.c
@@ -1,16 +1,22 @@
 #include "../minilibx_opengl_20191021/mlx.h"
 #include <stdio.h>
 
-int	world_map[8][8] = 
+typedef struct s_mlx
 {
-	{1, 1, 1, 1, 1, 1, 1, 1},
-	{1, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 0, 0, 1},
-	{1, 1, 1, 1, 1, 1, 1, 1}
+	void	*init_ptr;
+	void	*win;
+}	t_mlx;
+
+char	world_map[8][8] = 
+{
+	{'1', '1', '1', '1', '1', '1', '1', '1'},
+	{'1', '0', '0', '0', '0', '0', '0', '1'},
+	{'1', '0', '0', '0', '0', '0', '0', '1'},
+	{'1', '0', '0', '0', '0', '0', '0', '1'},
+	{'1', '0', '0', 'N', '0', '0', '0', '1'},
+	{'1', '0', '0', '0', '0', '0', '0', '1'},
+	{'1', '0', '0', '0', '0', '0', '0', '1'},
+	{'1', '1', '1', '1', '1', '1', '1', '1'},
 };
 
 int	key_press(int i, void *param)
@@ -22,20 +28,17 @@ int	key_press(int i, void *param)
 
 int	main(void)
 {
-	void	*mlx_ptr;
-	void	*mlx_win;
+	t_mlx	mlx;
 
 	for (int i = 0; i < 8; i++)
 	{
 		for(int j = 0; j < 8; j++)
-			printf("%d", world_map[i][j]);
+			printf("%c", world_map[i][j]);
 		printf("\n");
 	}
-	mlx_ptr = mlx_init();
-	printf("%p\n", mlx_ptr);
-	mlx_win = mlx_new_window(mlx_ptr, 640, 480, "cub3D");
-	mlx_hook(mlx_win, 2, 0, &key_press, NULL);
-	mlx_loop(mlx_ptr);
-	// printf("hello\n");
+	mlx.init_ptr = mlx_init();
+	mlx.win = mlx_new_window(mlx.init_ptr, 640, 480, "cub3D");
+	mlx_hook(mlx.win, 2, 0, &key_press, NULL);
+	mlx_loop(mlx.init_ptr);
 	return (0);
 }
