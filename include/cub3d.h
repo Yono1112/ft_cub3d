7f1c45690de9572ad5ca23f43a965f05bc18c825
Yono1112 <yuma.ohno.sss@gmail.com>
2023-10-25 20:19:23 +0900

    moved macros to defines.h.

--- include/cub3d.h
+++ include/cub3d.h
@@ -10,36 +10,7 @@
 # include <stdbool.h>
 # include <math.h>
 # include "../minilibx_opengl_20191021/mlx.h"
-
-# define MAP_WIDTH 16
-# define MAP_HEIGHT 16
-# define WINSIZE_WIDTH 400
-# define WINSIZE_HEIGTH 400
-# define SOUTH (M_PI / 180) * 90
-# define NORTH (M_PI / 180) * 270
-# define EAST (M_PI / 180) * 0
-# define WEST (M_PI / 180) * 180
-# define FOV (M_PI / 180) * 60
-# define SIDE_X 0
-# define SIDE_Y 1
-# define KEY_ESC 53
-# define KEY_W 13
-# define KEY_A 0
-# define KEY_S 1
-# define KEY_D 2
-# define KEY_ARROW_LEFT 123
-# define KEY_ARROW_RIGHT 124
-# define MOVE_SPEED 0.3
-# define TEX_WIDTH 64
-# define TEX_HEIGHT 64
-# define NORTH_TEX 0
-# define SOUTH_TEX 1
-# define EAST_TEX 2
-# define WEST_TEX 3
-# define MOVE_FORWARD 0
-# define MOVE_BACK 1
-# define MOVE_LEFT 2
-# define MOVE_RIGHT 3
+# include "defines.h"
 
 typedef struct s_map
 {
