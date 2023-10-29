76165cd03667a13f53b1ff31441cd3e30e6f83dc
Yono1112 <yuma.ohno.sss@gmail.com>
2023-10-29 17:50:35 +0900

    change the formula for draw_ceiling and draw_floor

--- src/mlx/draw/draw_ceiling.c
+++ src/mlx/draw/draw_ceiling.c
@@ -6,7 +6,7 @@
 /*   By: yumaohno <yumaohno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 02:41:47 by yumaohno          #+#    #+#             */
-/*   Updated: 2023/10/29 17:28:00 by yumaohno         ###   ########.fr       */
+/*   Updated: 2023/10/29 17:49:11 by yumaohno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	draw_ceiling(int x, int y, int ceiling_num[3], t_mlx *mlx)
 {
 	long	color_ceiling;
 
-	color_ceiling
-		= (ceiling_num[0] * 65536) + (ceiling_num[1] * 256) + ceiling_num[2];
+	color_ceiling = (ceiling_num[0] * 256 * 256)
+		+ (ceiling_num[1] * 256) + ceiling_num[2];
 	mlx_pixel_put(mlx->mlx_ptr, mlx->mlx_win, x, y, color_ceiling);
 }
--- src/mlx/draw/draw_floor.c
+++ src/mlx/draw/draw_floor.c
@@ -6,7 +6,7 @@
 /*   By: yumaohno <yumaohno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 02:41:27 by yumaohno          #+#    #+#             */
-/*   Updated: 2023/10/29 17:30:37 by yumaohno         ###   ########.fr       */
+/*   Updated: 2023/10/29 17:48:36 by yumaohno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void	draw_floor(int x, int y, int floor_num[3], t_mlx *mlx)
 {
 	long	color_floor;
 
-	color_floor = (floor_num[0] * 65536) + (floor_num[1] * 256) + floor_num[2];
+	color_floor
+		= (floor_num[0] * 256 * 256) + (floor_num[1] * 256) + floor_num[2];
 	mlx_pixel_put(mlx->mlx_ptr, mlx->mlx_win, x, y, color_floor);
 }
