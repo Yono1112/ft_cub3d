c50af17bc7c53c410f2cca970a5bcba4620911bf
Yono1112 <yuma.ohno.sss@gmail.com>
2023-11-02 20:20:38 +0900

    rm printf of press_key and comment out leaks

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: yumaohno <yumaohno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 16:35:00 by yumaohno          #+#    #+#             */
-/*   Updated: 2023/11/02 14:30:21 by yumaohno         ###   ########.fr       */
+/*   Updated: 2023/11/02 20:20:00 by yumaohno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,8 +34,8 @@ int	main(int argc, char **argv)
 	exit(EXIT_SUCCESS);
 }
 
-__attribute__((destructor))
-static void	destructor(void)
-{
-	system("leaks -q cub3D");
-}
+// __attribute__((destructor))
+// static void	destructor(void)
+// {
+// 	system("leaks -q cub3D");
+// }
