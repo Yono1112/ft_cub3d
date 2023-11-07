f7732671677105ef199b02860341873f397256c8
Yono1112 <yuma.ohno.sss@gmail.com>
2023-11-07 16:40:06 +0900

    refactor set_direction()

--- src/mlx/init/set_player.c
+++ src/mlx/init/set_player.c
@@ -6,45 +6,49 @@
 /*   By: yumaohno <yumaohno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 02:07:40 by yumaohno          #+#    #+#             */
-/*   Updated: 2023/11/07 15:29:26 by yumaohno         ###   ########.fr       */
+/*   Updated: 2023/11/07 16:39:29 by yumaohno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+static void	set_player_direction(t_mlx *mlx, double dir_x, double dir_y)
+{
+	mlx->dir_x = dir_x;
+	mlx->dir_y = dir_y;
+}
+
+static void	set_player_plane(t_mlx *mlx, double plane_x, double plane_y)
+{
+	mlx->plane_x = plane_x;
+	mlx->plane_y = plane_y;
+}
 
 static void	set_direction(t_mlx *mlx, char direction)
 {
 	if (direction == 'N')
 	{
 		mlx->player_direct = NORTH;
-		mlx->dir_x = 0;
-		mlx->dir_y = -1;
-		mlx->plane_x = 0.66;
-		mlx->plane_y = 0;
+		set_player_direction(mlx, 0, -1);
+		set_player_plane(mlx, 0.66, 0);
 	}
 	if (direction == 'S')
 	{
 		mlx->player_direct = SOUTH;
-		mlx->dir_x = 0;
-		mlx->dir_y = 1;
-		mlx->plane_x = -0.66;
-		mlx->plane_y = 0;
+		set_player_direction(mlx, 0, 1);
+		set_player_plane(mlx, -0.66, 0);
 	}
 	if (direction == 'E')
 	{
 		mlx->player_direct = EAST;
-		mlx->dir_x = 1;
-		mlx->dir_y = 0;
-		mlx->plane_x = 0;
-		mlx->plane_y = 0.66;
+		set_player_direction(mlx, 1, 0);
+		set_player_plane(mlx, 0, 0.66);
 	}
 	if (direction == 'W')
 	{
 		mlx->player_direct = WEST;
-		mlx->dir_x = -1;
-		mlx->dir_y = 0;
-		mlx->plane_x = 0;
-		mlx->plane_y = -0.66;
+		set_player_direction(mlx, -1, 0);
+		set_player_plane(mlx, 0, -0.66);
 	}
 }
 
