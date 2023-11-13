9a2fe2ec696d0bf8a4ced15a0f611c322519b0ec
Yono1112 <yuma.ohno.sss@gmail.com>
2023-11-13 18:18:32 +0900

    rm unnecessary files and mkdir map

--- src/main.c
+++ src/main.c
@@ -6,36 +6,35 @@
 /*   By: yumaohno <yumaohno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 16:35:00 by yumaohno          #+#    #+#             */
-/*   Updated: 2023/11/02 14:30:21 by yumaohno         ###   ########.fr       */
+/*   Updated: 2023/11/13 18:13:16 by yumaohno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"cub3d.h"
 
-void	print_map(char **map)
-{
-	int	i;
+// void	print_map(char **map)
+// {
+// 	int	i;
 
-	i = 0;
-	while (map[i])
-	{
-		printf("%s\n", map[i]);
-		i++;
-	}
-}
+// 	i = 0;
+// 	while (map[i])
+// 	{
+// 		printf("%s\n", map[i]);
+// 		i++;
+// 	}
+// }
 
 int	main(int argc, char **argv)
 {
 	t_map	mapdata;
 
 	check_error((const int)argc, (const char **)argv, &mapdata);
-	print_map(mapdata.map);
 	run_mlx(&mapdata);
 	exit(EXIT_SUCCESS);
 }
 
-__attribute__((destructor))
-static void	destructor(void)
-{
-	system("leaks -q cub3D");
-}
+// __attribute__((destructor))
+// static void	destructor(void)
+// {
+// 	system("leaks -q cub3D");
+// }
