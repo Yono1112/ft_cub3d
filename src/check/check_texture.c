3d39c0eefe9fecda34fd0b2966be387878d510dd
rnakaa <102458195+rnakaa@users.noreply.github.com>
2023-11-14 21:21:08 +0900

    fix \{remove space after texturepath\}

--- src/check/check_texture.c
+++ src/check/check_texture.c
@@ -6,7 +6,7 @@
 /*   By: rnaka <rnaka@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 19:45:16 by rnaka             #+#    #+#             */
-/*   Updated: 2023/11/12 14:56:04 by rnaka            ###   ########.fr       */
+/*   Updated: 2023/11/14 21:19:07 by rnaka            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,24 @@ void	initialize_texture(t_map *mapdata)
 	mapdata->floor = NULL;
 }
 
+void	remove_space_after_texture(char *line)
+{
+	int	i;
+	int	last_chr;
+
+	i = 0;
+	last_chr = 0;
+	if (!line)
+		return ;
+	while (line[i])
+	{
+		if (line[i] != ' ')
+			last_chr = i;
+		i++;
+	}
+	line[last_chr + 1] = '\0';
+}
+
 int	check_texture(char **map, t_map *mapdata)
 {
 	int	i;
@@ -48,5 +66,9 @@ int	check_texture(char **map, t_map *mapdata)
 	skip_space(map, &i, mapdata);
 	if (is_null_texture(mapdata))
 		exit_error(TEXTURE_ERROR, mapdata, map);
+	remove_space_after_texture(mapdata->no);
+	remove_space_after_texture(mapdata->so);
+	remove_space_after_texture(mapdata->ea);
+	remove_space_after_texture(mapdata->we);
 	return (i);
 }
